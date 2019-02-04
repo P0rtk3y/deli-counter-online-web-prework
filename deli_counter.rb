8be086fdbf2269471d36bf3ac8df89101cd65ac9
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-04 09:06:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  count = katz_deli.length
+  if count == 0 
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{katz_deli}"
