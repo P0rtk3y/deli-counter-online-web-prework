4d9927d2f3b37f9693619b7d3c1405063e4d4878
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-04 09:38:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,13 @@
 
 def line(katz_deli)
   line = ""
+  people_in_line = "The line is currently: "
   if katz_deli.empty?
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |name, index|
-      line += "#{index+1}. #{name}" 
+      line += "#{index+1}. #{name} " 
     end
+    puts people_in_line + line 
   end
 end
