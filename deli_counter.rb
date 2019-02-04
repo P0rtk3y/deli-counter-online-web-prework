04874339770028be084f810916263921245151e2
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-04 09:50:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,15 @@
 # Write your code here.
 
 def line(katz_deli)
-  line = ""
-  people_in_line = "The line is currently:"
+  add_line = ""
+  current_line = "The line is currently:"
   if katz_deli.empty?
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |name, index|
-      line += " #{index+1}. #{name}" 
+      add_line += " #{index+1}. #{name}" 
     end
-    puts people_in_line + line 
+    puts current_line + add_line 
   end
 end
 
