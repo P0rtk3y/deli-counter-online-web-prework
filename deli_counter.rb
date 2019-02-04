ea053949afcc237cc1c1d8ee9970a6100174e921
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-04 09:11:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,4 +6,5 @@ def line(katz_deli)
   if count == 0 
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{katz_deli}"
+    katz_deli.each_with_index do |name, index|
+      puts "The line is currently: #{index+1}.#{name}"
