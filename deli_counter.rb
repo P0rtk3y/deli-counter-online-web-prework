ffb109e389e0928471c15a3b814df49ac5a66f46
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-04 09:28:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,12 @@
 katz_deli = []
 
 def line(katz_deli)
-  string = "The line is currently:"
+  string = "The line is currently: "
   if katz_deli.empty?
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |name, index|
-      string.concat(#{index+1}. #{name})
+      
     end
   end
 end
